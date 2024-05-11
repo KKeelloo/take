ef4dc468610426650110e2abeef8567618735bcd
inf141291 <mateusz.ollek@student.put.poznan.pl>
2024-05-11 19:59:35 +0200

    Adding lab9 (agnular+ .NET)

---
+++ lab9/StudentsFront/src/app/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+  title = 'About'
+  date?: Date;
+  ngOnInit(){
+    this.date = new Date();
+  }
+}
